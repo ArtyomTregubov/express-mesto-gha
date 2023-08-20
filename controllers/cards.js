40c14166b17f2ebe64a90cbcb539eeee01c94c34
ArtyomTregubov <SemperIdem1989@gmail.com>
2023-08-20 19:15:33 +0500

    fix20

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,6 +4,7 @@ const {
   SUCCESS_CODE_200,
   ERROR_CODE_400, SUCCESS_CREATE_CODE_201, ERROR_NOT_FOUND_CODE_404,
 } = require('../const/errors_code');
+const User = require("../models/user");
 // DELETE /cards/:cardId — удаляет карточку по идентификатору
 const getCards = async (req, res) => {
   try {
@@ -14,15 +15,39 @@ const getCards = async (req, res) => {
   }
 };
 
-const likeCard = (req, res) => Card.findByIdAndUpdate(
-  req.query.cardId,
-  { $addToSet: { likes: req.query._id } },
+// const likeCard = (req, res) => Card.findByIdAndUpdate(
+//   req.query.cardId,
+//   { $addToSet: { likes: req.query._id } },
+//   { new: true },
+// ).then((like) => {
+//   res.status(SUCCESS_CODE_200).send({});
+// }).catch((err) => {
+//   res.status(ERROR_CODE_400).send(err);
+// });
+
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } },
   { new: true },
-).then((card) => {
-  res.status(SUCCESS_CODE_200).send(card);
+).then((like) => {
+  res.status(SUCCESS_CODE_200).send(like);
 }).catch((err) => {
   res.status(ERROR_CODE_400).send(err);
 });
+
+  // User.findByIdAndUpdate(id, params, { new: true, runValidators: true })
+  //   .then((user) => {
+  //     res.send(user);
+  //   })
+  //   .catch((err) => {
+  //     if (err.name === 'ValidationError') {
+  //       res.status(ERROR_CODE_400).send(err);
+  //       return;
+  //     }
+  //   res.status(ERROR_NOT_FOUND_CODE_404).send(err);
+  //   });
+};
 
 const dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.query.cardId,
