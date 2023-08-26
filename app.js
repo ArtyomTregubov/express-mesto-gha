c940f4edc8b3dcbb7e72164adbf9977b57ba28d6
ArtyomTregubov <SemperIdem1989@gmail.com>
2023-08-26 16:55:32 +0500

    fix32

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
-const errorHandling = require('./errors/errorHandling');
 const { unknownLink, createUser, login } = require('./controllers/users');
 
 const app = express();
@@ -14,8 +13,8 @@ app.use(bodyParser.json());
 
 const URI_MONGO = 'mongodb://localhost:27017/mestodb';
 
-mongoose.connect(URI_MONGO);
-// mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/mestodb?authSource=admin');
+// mongoose.connect(URI_MONGO);
+mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/mestodb?authSource=admin');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -31,5 +30,15 @@ app.use('/', routerUsers);
 app.use('/', routerCards);
 app.use('*', unknownLink);
 app.use(errors());
-app.use(errorHandling);
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
 app.listen(3000);
