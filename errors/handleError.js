1a72600f47cf974abedb6c1f318fedf0a08efae4
ArtyomTregubov <SemperIdem1989@gmail.com>
2023-09-02 20:22:30 +0500

    fix49

--- errors/handleError.js
+++ errors/handleError.js
@@ -1,11 +1,10 @@
 const handleError = (err, req, res, next) => {
-  let { statusCode = 500 } = err;
-  const { message } = err;
-  if (message === 'Неправильные почта или пароль') {
-    statusCode = 401;
-  } else if (err.name === 'ValidationError' || err.name === 'CastError') {
-    statusCode = 400;
-  }
+  const { message, statusCode = 500 } = err;
+  // if (message === 'Неправильные почта или пароль') {
+  //   statusCode = 401;
+  // } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+  //   statusCode = 400;
+  // }
 
   res
     .status(statusCode)
